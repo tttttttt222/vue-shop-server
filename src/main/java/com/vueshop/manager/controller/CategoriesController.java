de7c8cc184db8791d3987dbb5e4737c412ec9f19
yongwei_ren <yongwei_ren@baofu.com>
2020-03-08 23:08:19 +0800

    角色配置

--- src/main/java/com/vueshop/manager/controller/CategoriesController.java
+++ src/main/java/com/vueshop/manager/controller/CategoriesController.java
@@ -8,18 +8,11 @@ import com.vueshop.manager.controller.http.response.CategoriesInfoResponse;
 import com.vueshop.manager.controller.http.response.base.HttpResponse;
 import com.vueshop.manager.controller.http.response.base.Meta;
 import com.vueshop.manager.service.CategoriesService;
-import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 项目名称:vue-shop-manager 描述: 创建人:ryw 创建时间:2020/3/2
@@ -29,114 +22,120 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class CategoriesController {
 
-	@Autowired
-	private CategoriesService categoriesService;
+    @Autowired
+    private CategoriesService categoriesService;
 
-	@GetMapping("categories")
-	@ResponseBody
-	public HttpResponse<CategoriesInfoQueryResponse> queryCategoriesPage(HttpServletRequest request) {
-		HttpResponse<CategoriesInfoQueryResponse> httpResponse = new HttpResponse<>(new Meta());
+    @GetMapping("categories")
+    @ResponseBody
+    public HttpResponse<CategoriesInfoQueryResponse> queryCategoriesPage(HttpServletRequest request) {
+        HttpResponse<CategoriesInfoQueryResponse> httpResponse = new HttpResponse<>(new Meta());
 
-		String pagenum = request.getParameter("pagenum");
-		String pagesize = request.getParameter("pagesize");
-		String type = request.getParameter("type");
-		PageRequest<CategoriesInfoQueryRequest> pageRequest = new PageRequest(Integer.parseInt(pagenum),
-				Integer.parseInt(pagesize));
-		CategoriesInfoQueryRequest categoriesInfoQueryRequest = new CategoriesInfoQueryRequest();
-		categoriesInfoQueryRequest.setType(type);
-		pageRequest.setQuery(categoriesInfoQueryRequest);
-		CategoriesInfoQueryResponse categoriesInfoQueryResponse = categoriesService
-				.queryCategoriesPage(pageRequest);
+        String pagenum = request.getParameter("pagenum");
+        String pagesize = request.getParameter("pagesize");
+        String type = request.getParameter("type");
+        PageRequest<CategoriesInfoQueryRequest> pageRequest = null;
+        if (pagenum != null) {
+            pageRequest = new PageRequest(Integer.parseInt(pagenum),
+                    Integer.parseInt(pagesize));
+        } else {
+            pageRequest = new PageRequest();
+        }
 
-		httpResponse.setData(categoriesInfoQueryResponse);
-		httpResponse.getMeta().setMsg("获取商品分类列表成功");
-		httpResponse.getMeta().setStatus(200);
-		return httpResponse;
-	}
+        CategoriesInfoQueryRequest categoriesInfoQueryRequest = new CategoriesInfoQueryRequest();
+        categoriesInfoQueryRequest.setType(type);
+        pageRequest.setQuery(categoriesInfoQueryRequest);
+        CategoriesInfoQueryResponse categoriesInfoQueryResponse = categoriesService
+                .queryCategoriesPage(pageRequest);
 
-
-	@PostMapping("categories")
-	@ResponseBody
-	public HttpResponse<CategoriesInfoResponse> addCategories(HttpServletRequest request,
-			@RequestBody CategoriesInfoRequest categoriesInfoRequest) {
-		HttpResponse<CategoriesInfoResponse> httpResponse = new HttpResponse<>(new Meta());
-
-		CategoriesInfoResponse categoriesInfoResponse = categoriesService.insertCategories(categoriesInfoRequest);
-
-		if (categoriesInfoResponse == null) {
-			httpResponse.getMeta().setMsg("商品分类创建成功");
-			httpResponse.getMeta().setStatus(400);
-			return httpResponse;
-		}
-
-		httpResponse.setData(categoriesInfoResponse);
-		httpResponse.getMeta().setMsg("商品分类创建失败");
-		httpResponse.getMeta().setStatus(200);
-		return httpResponse;
-	}
+        httpResponse.setData(categoriesInfoQueryResponse);
+        httpResponse.getMeta().setMsg("获取商品分类列表成功");
+        httpResponse.getMeta().setStatus(200);
+        return httpResponse;
+    }
 
 
-	@GetMapping("categories/{id}")
-	@ResponseBody
-	public HttpResponse<CategoriesInfoResponse> queryCategoriesById(HttpServletRequest request, @PathVariable long id) {
-		HttpResponse<CategoriesInfoResponse> httpResponse = new HttpResponse<>(new Meta());
+    @PostMapping("categories")
+    @ResponseBody
+    public HttpResponse<CategoriesInfoResponse> addCategories(HttpServletRequest request,
+                                                              @RequestBody CategoriesInfoRequest categoriesInfoRequest) {
+        HttpResponse<CategoriesInfoResponse> httpResponse = new HttpResponse<>(new Meta());
 
-		CategoriesInfoResponse categoriesInfoResponse = categoriesService.queryCategoriesInfoById(id);
+        CategoriesInfoResponse categoriesInfoResponse = categoriesService.insertCategories(categoriesInfoRequest);
 
-		if (categoriesInfoResponse == null) {
-			httpResponse.getMeta().setMsg("查询失败");
-			httpResponse.getMeta().setStatus(400);
-			return httpResponse;
-		}
+        if (categoriesInfoResponse == null) {
+            httpResponse.getMeta().setMsg("商品分类创建成功");
+            httpResponse.getMeta().setStatus(400);
+            return httpResponse;
+        }
 
-		httpResponse.setData(categoriesInfoResponse);
-		httpResponse.getMeta().setMsg("查询成功");
-		httpResponse.getMeta().setStatus(200);
-		return httpResponse;
-	}
+        httpResponse.setData(categoriesInfoResponse);
+        httpResponse.getMeta().setMsg("商品分类创建失败");
+        httpResponse.getMeta().setStatus(200);
+        return httpResponse;
+    }
 
 
-	@PutMapping("categories/{id}")
-	@ResponseBody
-	public HttpResponse<CategoriesInfoResponse> updateCategoriesInfoById(HttpServletRequest request,
-			@PathVariable long id,
-			@RequestBody CategoriesInfoRequest CategoriesInfoRequest) {
-		HttpResponse<CategoriesInfoResponse> httpResponse = new HttpResponse<>(new Meta());
+    @GetMapping("categories/{id}")
+    @ResponseBody
+    public HttpResponse<CategoriesInfoResponse> queryCategoriesById(HttpServletRequest request, @PathVariable long id) {
+        HttpResponse<CategoriesInfoResponse> httpResponse = new HttpResponse<>(new Meta());
 
-		CategoriesInfoRequest.setCat_id(id);
-		CategoriesInfoResponse categoriesInfoResponse = categoriesService
-				.updateCategoriesInfoById(CategoriesInfoRequest);
+        CategoriesInfoResponse categoriesInfoResponse = categoriesService.queryCategoriesInfoById(id);
 
-		if (categoriesInfoResponse == null) {
-			httpResponse.getMeta().setMsg("更新失败");
-			httpResponse.getMeta().setStatus(400);
-			return httpResponse;
-		}
+        if (categoriesInfoResponse == null) {
+            httpResponse.getMeta().setMsg("查询失败");
+            httpResponse.getMeta().setStatus(400);
+            return httpResponse;
+        }
 
-		httpResponse.setData(categoriesInfoResponse);
-		httpResponse.getMeta().setMsg("更新成功");
-		httpResponse.getMeta().setStatus(200);
-		return httpResponse;
-	}
+        httpResponse.setData(categoriesInfoResponse);
+        httpResponse.getMeta().setMsg("查询成功");
+        httpResponse.getMeta().setStatus(200);
+        return httpResponse;
+    }
 
-	@DeleteMapping("categories/{id}")
-	@ResponseBody
-	public HttpResponse<CategoriesInfoResponse> deleteCategoriesById(HttpServletRequest request,
-			@PathVariable long id) {
-		HttpResponse<CategoriesInfoResponse> httpResponse = new HttpResponse<>(new Meta());
 
-		CategoriesInfoResponse categoriesInfoResponse = categoriesService.deleteCategoriesById(id);
+    @PutMapping("categories/{id}")
+    @ResponseBody
+    public HttpResponse<CategoriesInfoResponse> updateCategoriesInfoById(HttpServletRequest request,
+                                                                         @PathVariable long id,
+                                                                         @RequestBody CategoriesInfoRequest CategoriesInfoRequest) {
+        HttpResponse<CategoriesInfoResponse> httpResponse = new HttpResponse<>(new Meta());
 
-		if (categoriesInfoResponse == null) {
-			httpResponse.getMeta().setMsg("删除失败");
-			httpResponse.getMeta().setStatus(400);
-			return httpResponse;
-		}
+        CategoriesInfoRequest.setCat_id(id);
+        CategoriesInfoResponse categoriesInfoResponse = categoriesService
+                .updateCategoriesInfoById(CategoriesInfoRequest);
 
-		httpResponse.setData(categoriesInfoResponse);
-		httpResponse.getMeta().setMsg("删除成功");
-		httpResponse.getMeta().setStatus(200);
-		return httpResponse;
-	}
+        if (categoriesInfoResponse == null) {
+            httpResponse.getMeta().setMsg("更新失败");
+            httpResponse.getMeta().setStatus(400);
+            return httpResponse;
+        }
+
+        httpResponse.setData(categoriesInfoResponse);
+        httpResponse.getMeta().setMsg("更新成功");
+        httpResponse.getMeta().setStatus(200);
+        return httpResponse;
+    }
+
+    @DeleteMapping("categories/{id}")
+    @ResponseBody
+    public HttpResponse<CategoriesInfoResponse> deleteCategoriesById(HttpServletRequest request,
+                                                                     @PathVariable long id) {
+        HttpResponse<CategoriesInfoResponse> httpResponse = new HttpResponse<>(new Meta());
+
+        CategoriesInfoResponse categoriesInfoResponse = categoriesService.deleteCategoriesById(id);
+
+        if (categoriesInfoResponse == null) {
+            httpResponse.getMeta().setMsg("删除失败");
+            httpResponse.getMeta().setStatus(400);
+            return httpResponse;
+        }
+
+        httpResponse.setData(categoriesInfoResponse);
+        httpResponse.getMeta().setMsg("删除成功");
+        httpResponse.getMeta().setStatus(200);
+        return httpResponse;
+    }
 
 }
